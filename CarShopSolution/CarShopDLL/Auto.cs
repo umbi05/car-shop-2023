8c61de4c35f0fae5fb1058cff7c81b403820ea0f
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2021-04-07 11:17:02 +0200

    Initial solution with DLL project

---
+++ CarShopSolution/CarShopDLL/Auto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarShopDLL
+{
+    class Auto : Veicolo
+    {
+        public bool IsCabrio { get; set; }
+
+        public Auto(string marca, string modello, bool isCabrio)
+        {
+            Marca = marca;
+            Modello = modello;
+            IsCabrio = isCabrio;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            string st = Marca + " - " + Modello;
+            if (IsCabrio)
+            {
+                st += " (cabrio)";
+            }
+            return st;
+        }
+    }
+}
---
+++ CarShopSolution/CarShopDLL/Veicolo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CarShopDLL
+{
+    public abstract class Veicolo
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+    }
+}
