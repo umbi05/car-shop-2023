07f883f9290f05c6b3ae6d14b5abaf85524042e4
ProfOscar <61749199+ProfOscar@users.noreply.github.com>
2023-02-01 12:56:38 +0100

    Library project created

---
+++ CarShopLibrary/ClsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopLibrary
+{
+    public class ClsTest
+    {
+        public static string HelloWorld(string name)
+        {
+            return "Ciao " + name;
+        }
+    }
+}
